1f86eefa472ef18ac939e241e86bf6ff91f4498f
Shota Yamahira <seak0503@gmail.com>
2015-05-01 07:24:44 +0900

    13-1 before_action

--- app/controllers/admin/base.rb
+++ app/controllers/admin/base.rb
@@ -1,6 +1,7 @@
 class Admin::Base < ApplicationController
-  private
+  before_action :authorize
 
+  private
   def current_administrator
     if session[:administrator_id]
       @current_administrator ||=
@@ -9,4 +10,11 @@ class Admin::Base < ApplicationController
   end
 
   helper_method :current_administrator
+
+  def authorize
+    unless current_administrator
+      flash.alert = '管理者としてログインしてください。'
+      redirect_to :admin_login
+    end
+  end
 end
--- app/controllers/admin/staff_members_controller.rb
+++ app/controllers/admin/staff_members_controller.rb
@@ -1,4 +1,5 @@
 class Admin::StaffMembersController < Admin::Base
+
   def index
     @staff_members = StaffMember.order(:family_name_kana, :given_name_kana)
   end
@@ -45,11 +46,11 @@ class Admin::StaffMembersController < Admin::Base
   end
 
   private
-    def staff_member_params
-      params.require(:staff_member).permit(
-        :email, :password, :family_name, :given_name,
-        :family_name_kana, :given_name_kana,
-        :start_date, :end_date, :suspended
-      )
-    end
+  def staff_member_params
+    params.require(:staff_member).permit(
+      :email, :password, :family_name, :given_name,
+      :family_name_kana, :given_name_kana,
+      :start_date, :end_date, :suspended
+    )
+  end
 end
