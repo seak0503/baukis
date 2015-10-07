fd2fdd40a051dec0cd830451f3cd79188a73627c
Shota Yamahira <seak0503@gmail.com>
2015-10-07 19:28:17 +0900

    17-1 jQueryとCoffeeScript

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,1 +1,2 @@
-Rails.application.config.assets.precompile += %w( admin.css staff.css customer.css )
+Rails.application.config.assets.precompile +=
+  %w( admin.css staff.css customer.css staff.js admin.js customer.js )
