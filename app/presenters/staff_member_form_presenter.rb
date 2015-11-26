f2be4c36d02e5c0dac7fd4d8c9cfed4907bd4b5b
shyamahira <shyamahira@bbtower.co.jp>
2015-11-26 12:26:51 +0900

    bug fix

--- app/presenters/staff_member_form_presenter.rb
+++ app/presenters/staff_member_form_presenter.rb
@@ -1,7 +1,7 @@
 class StaffMemberFormPresenter < UserFormPresenter
   def check_boxes
     markup(:div, class: 'input-block') do |m|
-      m << check_boxe(:suspended)
+      m << check_box(:suspended)
       m << label(:suspended, 'アカウント停止')
     end
   end
