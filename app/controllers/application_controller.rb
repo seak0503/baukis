c9225f02f04ec798631ab89bdcee43090b6a4da9
shyamahira <shyamahira@bbtower.co.jp>
2016-03-10 06:38:57 +0900

    第８章 Ajax 完了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,8 @@ class ApplicationController < ActionController::Base
   class Forbidden < ActionController::ActionControllerError; end
   class IpAddressRejected < ActionController::ActionControllerError; end
 
-  include ErrorHandlers if Rails.env.production?
+  #include ErrorHandlers if Rails.env.production?
+  include ErrorHandlers
   rescue_from ApplicationController::Forbidden, with: :rescue403
   rescue_from ApplicationController::IpAddressRejected, with: :rescue403
 
@@ -24,5 +25,9 @@ class ApplicationController < ActionController::Base
   def rescue403(e)
     @exception = e
     render 'errors/forbidden', status: 403
+  end
+
+  def reject_non_xhr
+    raise ActionController::BadRequest unless request.xhr?
   end
 end
--- app/controllers/staff/messages_controller.rb
+++ app/controllers/staff/messages_controller.rb
@@ -1,4 +1,5 @@
 class Staff::MessagesController < Staff::Base
+  before_action :reject_non_xhr, only: [ :count ]
   # GET
   def count
     render text: CustomerMessage.unprocessed.count
