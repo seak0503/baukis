f112edd2d30d061f34c7967ab5f49254560009bd
shyamahira <shyamahira@bbtower.co.jp>
2016-07-04 05:56:43 +0900

    10.2.7 完了

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160527223940) do
+ActiveRecord::Schema.define(version: 20160703204644) do
 
   create_table "addresses", force: true do |t|
     t.integer  "customer_id",                null: false
