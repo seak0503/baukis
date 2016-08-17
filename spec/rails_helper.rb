58dd5b78202541852a4990333dc078dac15d893f
Shota Yamahira <seak0503@gmail.com>
2016-08-18 06:58:32 +0900

    railsの設定を初期状態に戻す

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -11,21 +11,4 @@ RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
-  config.filter_run_excluding :performance => true
-
-  config.include FactoryGirl::Syntax::Methods
-
-  config.before(:suite) do
-    FactoryGirl.reload
-  end
-
-  config.before(performance: true) do
-    ActionController::Base.perform_caching = true
-    ActiveSupport::Dependencies.mechanism = :require
-    Rails.logger.level = ActiveSupport::Logger::INFO
-  end
-
-  config.after do
-    Rails.application.config.baukis[:restrict_ip_addresses] = false
-  end
 end
