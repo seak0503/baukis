edf9c7c004b559df050d0507b46abf41aa255563
Shota Yamahira <seak0503@gmail.com>
2016-08-21 08:40:14 +0900

    chapter12完了

--- app/controllers/staff/accounts_controller.rb
+++ app/controllers/staff/accounts_controller.rb
@@ -22,13 +22,9 @@ class Staff::AccountsController < Staff::Base
   def update
     @staff_member = current_staff_member
     @staff_member.assign_attributes(staff_member_params)
-    if params[:commit]
-      if @staff_member.save
-        flash.notice = 'アカウント情報を更新しました。'
-        redirect_to :staff_account
-      else
-        render action: 'edit'
-      end
+    if @staff_member.save
+      flash.notice = 'アカウント情報を更新しました。'
+      redirect_to :staff_account
     else
       render action: 'edit'
     end
