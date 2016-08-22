043cd0c8fdd1cb2e254d14991223f9fc94263ac8
Shota Yamahira <seak0503@gmail.com>
2016-08-23 07:12:39 +0900

    Chapter15完了

--- app/controllers/staff/top_controller.rb
+++ app/controllers/staff/top_controller.rb
@@ -2,10 +2,6 @@ class Staff::TopController < Staff::Base
   skip_before_action :authorize
 
   def index
-    if current_staff_member
-      render action: 'dashboard'
-    else
-      render action: 'index'
-    end
+    render action: 'index'
   end
 end
---
+++ spec/support/shared_examples_for_staff_controllers.rb
@@ -0,0 +1,23 @@
+shared_examples 'a protected staff controller' do
+  describe '#index' do
+    example 'ログインフォームにリダイレクト' do
+      expect(response).to redirect_to(staff_login_url)
+    end
+  end
+
+  describe '#show' do
+    example 'ログインフォームにリダイレクト' do
+      get :show, id: 1
+      expect(response).to redirect_to(staff_login_url)
+    end
+  end
+end
+
+shared_examples 'a protected singular staff controller' do
+  describe '#show' do
+    example 'ログインフォームにリダイレクト' do
+      get :show
+      expect(response).to redirect_to(staff_login_url)
+    end
+  end
+end
