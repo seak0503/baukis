8f04264e3173398a23f773354d5b5567eaeca08d
shyamahira <shyamahira@bbtower.co.jp>
2016-08-30 09:46:22 +0900

    beforeブロックの位置を統一化

--- spec/controllers/staff/top_controller_spec.rb
+++ spec/controllers/staff/top_controller_spec.rb
@@ -26,13 +26,14 @@ describe Staff::TopController do
   context "ログイン後" do
     let(:staff_member) { create(:staff_member) }
 
-    before do
-      Rails.application.config.baukis[:restrict_ip_addresses] = false
-      session[:staff_member_id] = staff_member.id
-      session[:last_access_time] = 1.second.ago
-    end
-
     describe '#index' do
+
+      before do
+        Rails.application.config.baukis[:restrict_ip_addresses] = false
+        session[:staff_member_id] = staff_member.id
+        session[:last_access_time] = 1.second.ago
+      end
+
       example '通常はstaff/top/dashboardを表示' do
         get :index
         expect(response).to render_template('staff/top/dashboard')
